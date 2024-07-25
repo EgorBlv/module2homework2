5b04ba53f6abdf999e09151f0800421ccfa84a16
EgorBlv <egor.zuev1@yandex.ru>
2024-07-25 17:58:39 +0300

    Add files via upload

---
+++ module2homework2.py
@@ -0,0 +1,20 @@
+first = 123 # 1 задача
+second = 456
+third = 789
+if first == second == third:
+    print(3)
+elif first == second or third == second or first == third:
+    print(2)
+elif not first == second == third:
+    print(0)
+
+
+first = 42 # 2 задача
+second = 69
+third = 42
+if first == second == third:
+    print(3)
+elif first == second or third == second or first == third:
+    print(2)
+elif not first == second == third:
+    print(0)
